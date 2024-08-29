2ab544d02f0c28e70b17ec07caf9f368638f9bae
Serhii Martynov <sergei.mmssaa@gmail.com>
2024-08-29 18:40:18 +0300

    Add handleScroll

--- src/assets/scripts/scripts.js
+++ src/assets/scripts/scripts.js
@@ -4,4 +4,39 @@ $(document).ready(() => {
     $('.header__mobile-nav').on('click', () => {
         $('.header').toggleClass('active');
     })
+
+    $('.header__nav-link').on('click', (e) => {
+        $('.header__nav-link').removeClass('active');
+        const headerHeight = $('.header').outerHeight();
+
+        $(e.currentTarget).addClass('active');
+        const targetId = $(e.currentTarget).attr('href');
+        const targetSection = $(`${targetId}`);
+        console.log(headerHeight);
+
+        if (targetSection.length) {
+            targetSection.scrollIntoView({ behavior: 'smooth' });
+        } else {
+            console.error(`Current section with ID "${targetId}" unfinded`);
+        }
+    })
+
+    const handleScroll = () => {
+        $('.main__item').each(function () {
+            const $section = $(this);
+            const sectionTop = $section.offset().top;
+            const sectionHeight = $section.outerHeight();
+            console.log(sectionTop);
+
+            if ($(window).scrollTop() >= sectionTop - (sectionHeight * 0.5)) {
+                const sectionId = $section.attr('id');
+                const menuItem = $(`${'.header__nav-link'}[href="#${sectionId}"]`);
+
+                $('.header__nav-link').removeClass('active');
+                menuItem.addClass('active');
+            }
+        });
+    }
+
+    $(window).on('scroll', handleScroll);
 })
