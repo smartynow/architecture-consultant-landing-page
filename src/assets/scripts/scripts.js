6feb490dfbfc0105c7fecb85d022abe57fb2706b
Serhii Martynov <sergei.mmssaa@gmail.com>
2024-08-31 21:54:25 +0300

    Add smoothScroll for mobile

--- src/assets/scripts/scripts.js
+++ src/assets/scripts/scripts.js
@@ -20,43 +20,161 @@ $(document).ready(() => {
         speed: 500,
     });
     //Navigation
-    $('.nav__btn').on('click', () => {
-        $('.header').toggleClass('active');
-    })
+    // $('.nav__btn').on('click', () => {
+    //     $('.header').toggleClass('active');
+    // })
+    //
+    // $('.nav__link').on('click', (e) => {
+    //     $('.nav__link').removeClass('active');
+    //     const headerHeight = $('.header').outerHeight();
+    //     $('.header').removeClass('active');
+    //
+    //     $(e.currentTarget).addClass('active');
+    //     const targetId = $(e.currentTarget).attr('href');
+    //     const targetSection = $(`${targetId}`);
+    //
+    //     if (targetSection.length) {
+    //         $('html, body').animate({
+    //             scrollTop: targetSection.offset().top - $('.header').outerHeight()
+    //         }, 500); // Adjust the duration (in milliseconds) as needed
+    //     } else {
+    //         console.error(`Current section with ID "${targetId}" undefined`);
+    //     }
+    // })
+    //
+    // const handleScroll = () => {
+    //     $('.main__item').each(function () {
+    //         const $section = $(this);
+    //         const sectionTop = $section.offset().top;
+    //         const sectionHeight = $section.outerHeight();
+    //
+    //         if ($(window).scrollTop() >= sectionTop - (sectionHeight * 0.5)) {
+    //             const sectionId = $section.attr('id');
+    //             const menuItem = $(`${'.nav__link'}[href="#${sectionId}"]`);
+    //
+    //             $('.nav__link').removeClass('active');
+    //             menuItem.addClass('active');
+    //         }
+    //     });
+    // }
+    //
+    // $(window).on('scroll', handleScroll);
 
-    $('.nav__link').on('click', (e) => {
-        $('.nav__link').removeClass('active');
-        const headerHeight = $('.header').outerHeight();
-        $('.header').removeClass('active');
+    class Navigation {
+        constructor(headerSelector = ".header", navSelector = ".nav") {
+            // Select header element
+            this.header = document.querySelector(headerSelector);
+            if (!this.header)
+                throw new Error(
+                    `Navigation element with selector "${headerSelector}" not found`
+                );
+            // Select main navigation element
+            this.nav = document.querySelector(navSelector);
+            if (!this.nav)
+                throw new Error(
+                    `Navigation element with selector "${navSelector}" not found`
+                );
 
-        $(e.currentTarget).addClass('active');
-        const targetId = $(e.currentTarget).attr('href');
-        const targetSection = $(`${targetId}`);
+            // Select key elements
+            this.hamburger = this.header.querySelector(".nav__btn");
+            this.navContainer = this.nav.querySelector(".nav__container");
+            this.navItemsLeft = this.nav.querySelector(".nav__items_left");
+            this.navItemsRight = this.nav.querySelector(".nav__items_right");
 
-        if (targetSection.length) {
-            $('html, body').animate({
-                scrollTop: targetSection.offset().top - $('.header').outerHeight()
-            }, 500); // Adjust the duration (in milliseconds) as needed
-        } else {
-            console.error(`Current section with ID "${targetId}" undefined`);
+            // Define class names for mobile menu
+            this.mobileMenuClass = "nav__items_mobile";
+            this.activeClass = "active";
+
+            this.init();
         }
-    })
 
-    const handleScroll = () => {
-        $('.main__item').each(function () {
-            const $section = $(this);
-            const sectionTop = $section.offset().top;
-            const sectionHeight = $section.outerHeight();
-
-            if ($(window).scrollTop() >= sectionTop - (sectionHeight * 0.5)) {
-                const sectionId = $section.attr('id');
-                const menuItem = $(`${'.nav__link'}[href="#${sectionId}"]`);
-
-                $('.nav__link').removeClass('active');
-                menuItem.addClass('active');
+        // Initialize event listeners
+        init() {
+            if (window.innerWidth < 992) {
+                this.hamburger.addEventListener("click", () => {
+                    this.toggleMobileMenu();
+                });
+            } else {
+                this.hamburger.removeEventListener("click", () => {
+                    this.toggleMobileMenu();
+                });
             }
-        });
+        }
+
+        // Toggle mobile menu visibility
+        toggleMobileMenu() {
+            this.hamburger.classList.toggle("nav__btn_active");
+
+            if (this.hamburger.classList.contains("nav__btn_active")) {
+                this.createMobileMenu();
+            } else {
+                this.removeMobileMenu();
+            }
+        }
+
+        smoothScroll(event) {
+            const target = event.target;
+
+            if (target.closest(".nav__link")) {
+                const targetId = target.getAttribute("href");
+                const targetSection = document.querySelector(targetId);
+                if (targetSection) {
+                    event.preventDefault();
+                    targetSection.scrollIntoView({ behavior: 'smooth' });
+                    this.toggleMobileMenu();
+                } else {
+                    console.error(`Current section with ID "${targetId}" undefined`);
+                }
+            }
+        }
+
+        // Create and display mobile menu
+        createMobileMenu() {
+            const allNavItems = [
+                ...this.navItemsLeft.children,
+                ...this.navItemsRight.children
+            ];
+            const mobileNavItems = document.createElement("ul");
+            mobileNavItems.className = `nav__items ${this.mobileMenuClass}`;
+
+            // Clone nav items and add to mobile menu
+            allNavItems.forEach((item, index) => {
+                const clonedItem = item.cloneNode(true);
+                clonedItem.style.setProperty("--item-index", index + 1);
+                mobileNavItems.appendChild(clonedItem);
+            });
+
+            this.navContainer.appendChild(mobileNavItems);
+
+            // Force reflow to ensure the initial state is applied before adding the 'active' class
+            mobileNavItems.offsetHeight;
+            this.header.classList.add(this.activeClass);
+            document.body.style.overflow = "hidden";
+
+            mobileNavItems.addEventListener("click", (event) => this.smoothScroll(event));
+        }
+
+        // Remove mobile menu
+        removeMobileMenu() {
+            const mobileNavItems = this.nav.querySelector(`.${this.mobileMenuClass}`);
+            document.body.style.overflow = "auto";
+
+            if (mobileNavItems) {
+                this.header.classList.remove(this.activeClass);
+
+                setTimeout(() => {
+                    mobileNavItems.remove();
+                    mobileNavItems.removeEventListener("click", (event) => this.smoothScroll(event));
+                }, 500);
+            }
+        }
     }
 
-    $(window).on('scroll', handleScroll);
+    // Usage
+    try {
+        const navigation = new Navigation();
+    } catch (error) {
+        console.error("Failed to initialize navigation:", error.message);
+    }
+
 })
