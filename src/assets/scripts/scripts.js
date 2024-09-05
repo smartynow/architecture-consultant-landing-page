ac501c9bb26d75b3f64a96d73bfab2ea720c9c2c
smartynow <sergei.mmssaa@gmail.com>
2024-09-05 19:50:26 +0300

    Add inputmask in modal window

--- src/assets/scripts/scripts.js
+++ src/assets/scripts/scripts.js
@@ -21,46 +21,9 @@ $(document).ready(() => {
         zoom: true,
         speed: 500,
     });
-    //Navigation
-    // $('.nav__btn').on('click', () => {
-    //     $('.header').toggleClass('active');
-    // })
-    //
-    // $('.nav__link').on('click', (e) => {
-    //     $('.nav__link').removeClass('active');
-    //     const headerHeight = $('.header').outerHeight();
-    //     $('.header').removeClass('active');
-    //
-    //     $(e.currentTarget).addClass('active');
-    //     const targetId = $(e.currentTarget).attr('href');
-    //     const targetSection = $(`${targetId}`);
-    //
-    //     if (targetSection.length) {
-    //         $('html, body').animate({
-    //             scrollTop: targetSection.offset().top - $('.header').outerHeight()
-    //         }, 500); // Adjust the duration (in milliseconds) as needed
-    //     } else {
-    //         console.error(`Current section with ID "${targetId}" undefined`);
-    //     }
-    // })
-    //
-    // const handleScroll = () => {
-    //     $('.main__item').each(function () {
-    //         const $section = $(this);
-    //         const sectionTop = $section.offset().top;
-    //         const sectionHeight = $section.outerHeight();
-    //
-    //         if ($(window).scrollTop() >= sectionTop - (sectionHeight * 0.5)) {
-    //             const sectionId = $section.attr('id');
-    //             const menuItem = $(`${'.nav__link'}[href="#${sectionId}"]`);
-    //
-    //             $('.nav__link').removeClass('active');
-    //             menuItem.addClass('active');
-    //         }
-    //     });
-    // }
-    //
-    // $(window).on('scroll', handleScroll);
+
+    // Initialize Inputmask
+    $('#tel').inputmask({"mask": "(999) 999-9999"});
 
     function debounce(func, delay) {
         let timeoutId;
